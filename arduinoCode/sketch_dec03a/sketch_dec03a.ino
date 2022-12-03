9b5be75928f8d06cde396dd99053c48f9a7cb42a
Henry Ruiz <henryruiz22@gmail.com>
2022-12-03 13:10:33 -0600

    arduino code refactoring

--- arduinoCode/sketch_dec03a/sketch_dec03a.ino
+++ arduinoCode/sketch_dec03a/sketch_dec03a.ino
@@ -1,8 +1,16 @@
 #include "Servo.h"
+
+const int MAX_NUM_COMMANDS = 10;
+String commands[MAX_NUM_COMMANDS];
+
 Servo link1, link2;
+
+String commandText;
+bool commandReceived = false;
 
 
 void setup() {
+  // setup servos
   Serial.begin(9600);
   Serial.setTimeout(100);
   link1.attach(8);
@@ -10,55 +18,54 @@ void setup() {
   link1.write(90);
   link2.write(90);
 }
-String actions;
-bool commandReceived = false;
 
 void loop() {
 
-   
-  if(Serial.available()>0){
+  // read data send to the arduino using Serial Port
+  if(Serial.available()){
     commandReceived = true;
-    actions = Serial.readString();
+    commandText = Serial.readString();
+  }
+  
+  // process command and update ar, location based on the angles
+  if(commandReceived){
     Serial.flush();
-  }
-  
-  if(commandReceived)
-  {
-    //delay(100);
-    Serial.print(actions);
+    Serial.print(commandText);
     commandReceived = false;
-    split_string(actions);
     
+    split(commandText, ',');
+    
+    int angle1 = commands[0].toInt();
+    int angle2 = (180-commands[1].toInt());
+    setArmCoordinates(angle1,angle2);
   }
-  
+
+}
+
+void setArmCoordinates(int angle1, int angle2){
+  link1.write(angle1);
+  link2.write(angle2);
 }
 
 
-void split_string(String input)
+void split(String inputStr, char sep)
 {
-  String pieces[10];
-  uint8_t counter = 0;
-  uint8_t last_index = 0;
-  for (int i = 0; i < input.length(); i++) {
-    // Loop through each character and check if it's a comma
-    if (input.substring(i, i + 1) == ",") {
+  uint8_t currSubStrIdx = 0;
+  uint8_t lastSubStrIdx = 0;
+  for (int i = 0; i < inputStr.length(); i++) {
+    // Loop through each character and check for
+    if (inputStr.charAt(i) == sep) {
       // Grab the piece from the last index up to the current position and store it
-      pieces[counter] = input.substring(last_index, i);
+      commands[currSubStrIdx] = inputStr.substring(lastSubStrIdx, i);
       // Update the last position and add 1, so it starts from the next character
-      last_index = i + 1;
+      lastSubStrIdx = i + 1;
       // Increase the position in the array that we store into
-      counter++;
+      currSubStrIdx++;
     }
     // If we're at the end of the string (no more commas to stop us)
-    if (i == input.length() - 1) {
+    if (i == inputStr.length() - 1) {
       // Grab the last part of the string from the lastIndex to the end
-      pieces[counter] = input.substring(last_index, i + 1);
-      //counter++;
+      commands[currSubStrIdx] = inputStr.substring(lastSubStrIdx, i + 1);
     }
   }
-
-  int angle1 = pieces[0].toInt();
-  link1.write(angle1);
-  int angle2 = (180-pieces[1].toInt());
-  link2.write(angle2);
 }
